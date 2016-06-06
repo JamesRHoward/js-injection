ca6a703263400228919a078e4caf2f5de5f3aa68
James Howard and Patrick Lipscomb <james@>
2016-06-06 15:06:31 -0700

    change the completion correctness.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-  $('#flavors').submit(function(event) {
+  $('#btn').click(function(event) {
         event.preventDefault();
 debugger;
     var flavors = ['flavor1', 'flavor2', 'flavor3'];
 
+    var counter = 0;
     flavors.forEach(function(flavor) {
-      var userInput = $('input#' + flavor).val();
-      $('.' + flavor).text(userInput);
+      $("#results").append("<li>" + flavors[counter] + "</li>");
+      counter += 1;
     });
 
     $('#results').show();
